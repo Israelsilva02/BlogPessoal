51a82caffa1285534cbe09ba87ff29063a39cd0d
Israelsilva02 <israel865974@gmail.com>
2023-07-05 17:12:58 -0300

    Atualização Projeto

--- src/main/java/com/generation/blogPessoal/model/Postagem.java
+++ src/main/java/com/generation/blogPessoal/model/Postagem.java
@@ -31,6 +31,16 @@ public class Postagem {
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no minimo 10 e no maximo 1000 caracteres")
 	private String 	texto;
 	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 	
 	public long getId() {
 		return id;
@@ -59,18 +69,11 @@ public class Postagem {
 	public LocalDateTime getData() {
 		return data;
 	}
-
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
 
-	@UpdateTimestamp
-	private LocalDateTime data;
-
-	@ManyToOne
-	@JsonIgnoreProperties("postagens")
-	private Tema tema;
-
+	
 
 	public Tema getTema() {
 		return tema;
@@ -78,6 +81,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	}

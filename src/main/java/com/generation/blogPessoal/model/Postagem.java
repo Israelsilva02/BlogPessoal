212104bebe1b306f99bdb66bf854816c6d5545f0
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-03-26 17:50:53 -0300

    Update Postagem.java

--- src/main/java/com/generation/blogPessoal/model/Postagem.java
+++ src/main/java/com/generation/blogPessoal/model/Postagem.java
@@ -14,7 +14,8 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
+@Getter
+@Setter	
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
@@ -41,54 +42,6 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
-	
-	public long getId() {
-		return id;
-	}
 
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getTexto() {
-		return texto;
-	}
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-
-	public LocalDateTime getData() {
-		return data;
-	}
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-
-	
-
-	public Tema getTema() {
-		return tema;
-	}
-
-	public void setTema(Tema tema) {
-		this.tema = tema;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
 	
 	}
